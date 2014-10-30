cf17044b6a6665e6769aa2397430cb9918bcfdcc
Josh Wilhelm <wilhelm.josh@gmail.com>
2014-10-29 23:34:42 -0400

    Studio settings

---
+++ lisp/init-local.el
@@ -0,0 +1,8 @@
+```el
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/replace-colorthemes")
+
+(load-theme 'Deviant t)
+
+(provide 'init-local)
