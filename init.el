ee14042822168a794e8394ec4887624062274e91
Josh Wilhelm <wilhelm.josh@gmail.com>
2015-03-19 19:34:44 -0400

    commented some things that were causing errors.

--- init.el
+++ init.el
@@ -154,17 +154,17 @@
 ;; Mine! =====================================================================
 
 ;; load python-django.el to make working with django projects better
-(add-to-list 'load-path "~/.emacs.d/python-django.el")
-(require 'python-django)
-(global-set-key (kbd "C-c j") 'python-django-open-project)
+;; (add-to-list 'load-path "~/.emacs.d/python-django.el")
+;; (require 'python-django)
+;; (global-set-key (kbd "C-c j") 'python-django-open-project)
 
 ;; load python.el
 (add-to-list 'load-path "~/.emacs.d/python.el")
 (require 'python)
 
 ;; load pony-mode for editing django files
-(add-to-list 'load-path "~/.emacs.d/pony-mode/src")
-(require 'pony-mode)
+;; (add-to-list 'load-path "~/.emacs.d/pony-mode/src")
+;; (require 'pony-mode)
 
 ;; Opens my Studio timelog as a buffer on startup.
 ;; (find-file "~/Projects/org/timelogs/timelog-jwilhelm.org")
